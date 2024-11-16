3fa2e6a3e079b62032f5a79a3faf8d21805ed6bf
BunRealCua123 <hieukv26102003@gmail.com>
2024-11-16 13:51:33 +0700

    update

--- src/model/User.py
+++ src/model/User.py
@@ -1,11 +1,13 @@
 class User:
-    def __init__(self, name, username, password, image1 = '', image2 = '', role = 'user'):
+    def __init__(self, name, username, password, image1 = '', image2 = '', role = 'user', idImage1 = '', idImage2 = ''):
         self.name = name
         self.username = username
         self.password = password
         self.role = role
         self.image1 = image1
         self.image2 = image2
+        self.idImage1 = idImage1
+        self.idImage2 = idImage2
 
     def to_schema(self):
         return {
@@ -14,5 +16,7 @@ class User:
             'password': self.password,
             'role': self.role,
             'image1': self.image1,
-            'image2': self.image2
+            'image2': self.image2,
+            'idImage1': self.idImage1,
+            'idImage2': self.idImage2
         }
