28051847cab41d06a996d14e11402c4013c5f929
nt.hoa141203@gmail.com <nt.hoa141203@gmail.com>
2024-11-17 08:47:31 +0700

    chart

--- src/services/lightServices.py
+++ src/services/lightServices.py
@@ -2,6 +2,7 @@ from db import getDb
 import datetime
 from model.Led import Led
 from services.deviceService import updateState
+import json
 
 db = getDb()
 collection = db["ControlLight"]
@@ -15,3 +16,8 @@ def getLightState(deviceId):
     data = collection.find_one({'deviceId': deviceId}, sort=[('timestamp', -1)])
     led = Led(data['deviceId'], data['status'], data['timestamp'])
     return led
+
+def getLog(deviceId):
+    query = {"deviceId": deviceId}
+    devices = list(collection.find(query).sort("timestamp", -1).limit(5)) 
+    return json.loads(json.dumps(devices, default=str))
